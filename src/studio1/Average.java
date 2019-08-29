a3d6598c8241f661b36e3bc7ebabf8d0c935ed39
monica194 <monicahuang194@gmail.com>
2019-08-29 17:07:42 -0500

    finished studio 1

---
+++ src/studio1/Average.java
@@ -0,0 +1,17 @@
+package studio1;
+
+import support.cse131.ArgsProcessor;
+
+public class Average {
+
+    public static void main(String[] args){
+        ArgsProcessor ap = new ArgsProcessor(args);
+        int n1 = ap.nextInt("The first of two integers to be averaged?");
+        int n2 = ap.nextInt("The second of two integers to be averaged??");
+
+        System.out.println("The average of the two integers is " + (n1+n2)/2 );
+
+
+
+    }
+}
--- src/studio1/HiFour.java
+++ src/studio1/HiFour.java
@@ -4,7 +4,7 @@ import support.cse131.ArgsProcessor;
 
 /**
  * From Sedgewick and Wayne, COS 126 course at Princeton
- * 
+ *
  */
 public class HiFour {
 	public static void main(String[] args) {
@@ -16,6 +16,7 @@ public class HiFour {
 		//
 		// Say hello to the names in s0 through s3.
 		//
+		System.out.println("Greeting " + s0 + ", " + s1 + ", " + s2 +", " + s3 + ". ");
 
 	}
 }
